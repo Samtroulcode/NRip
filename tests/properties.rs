d1d1ce36d8fc4ba4c2ec5a4451851bf0ff6e6ee5
DuarteAd <adrien.duarte65@laposte.net>
2025-08-25 16:57:05 +0200

    fix : testing fzf

--- tests/properties.rs
+++ tests/properties.rs
@@ -2,10 +2,15 @@ use assert_cmd::prelude::*;
 use assert_fs::prelude::*;
 use proptest::prelude::*;
 use serial_test::serial;
-mod util;
+use std::process::Command;
 
 proptest! {
-  #![proptest_config(ProptestConfig::with_cases(64))]
+  #![proptest_config(ProptestConfig {
+      cases: 64,
+      // (facultatif) désactiver la persistence des échecs
+      failure_persistence: Some(Box::new(proptest::test_runner::FileFailurePersistence::Off)),
+      .. ProptestConfig::default()
+  })]
   #[test]
   #[serial]
   fn bury_then_resurrect_restores_original_path(file_name in "[a-zA-Z0-9._ -]{1,40}") {
@@ -13,21 +18,26 @@ proptest! {
     let src = tmp.child(&file_name);
     src.write_str("data").unwrap();
 
-    util::set_var("HOME", tmp.path());
-    util::set_var("XDG_DATA_HOME", tmp.child(".xdg/data").path());
-    util::set_var("XDG_CONFIG_HOME", tmp.child(".xdg/config").path());
+    // Isoler l'env
+    unsafe {
+      std::env::set_var("HOME", tmp.path());
+      std::env::set_var("XDG_DATA_HOME", tmp.child(".xdg/data").path());
+      std::env::set_var("XDG_CONFIG_HOME", tmp.child(".xdg/config").path());
+    }
 
-    std::process::Command::cargo_bin("nrip").unwrap()
+    // bury
+    Command::cargo_bin("nrip").unwrap()
         .arg(src.path())
         .assert()
         .success();
 
-    std::process::Command::cargo_bin("nrip").unwrap()
-        .args(["--resurrect"])
+    // resurrect NON-INTERACTIF : on passe la cible + -y pour éviter le prompt
+    Command::cargo_bin("nrip").unwrap()
+        .args(["-r", file_name.as_str(), "-y"])
         .assert()
         .success();
 
+    // Vérifie que le fichier est restauré
     src.assert(predicates::path::exists());
   }
 }
-
